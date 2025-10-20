15b83ecf1cdd491c3149d0016e3ca1687eb357a7
liannekristinedocena <docena.lianne.kristine@gmail.com>
2025-10-20 16:15:32 +0400

    Add files via upload

---
+++ EX5 DAYS OF THE MONTH.py
@@ -0,0 +1,58 @@
+#EX 5 DAYS OF THE MONTH 
+
+months = {1 : 31, #puts the days of the months into a dictionary with the month being the key and the days being the value 
+          2 : 28, 
+          3 : 31, 
+          4 : 30, 
+          5 : 31, 
+          6 : 30,
+          7 : 31, 
+          8 : 31, 
+          9 : 30, 
+          10 : 31, 
+          11 : 30, 
+          12 : 31}
+
+months2 = input("Input the month number! : ") #asks the user to input a number from the dictionary (1-12)
+
+if months2.isdigit(): #using .isdigits(): helps check if all characters in a string are classified as integers / digits 
+    monthnumber = int(months2) #converts the string input into an integer and places it in a new variable named 'monthnumber' 
+    if 1<= monthnumber <=12 : #checks whether or not the user input is within the range 1-12 
+        print(f"Month {monthnumber} has {months[monthnumber]} days.") #prints the month number as well as the month days from the dictionary 
+    else: 
+        print("That is not a valid month") #displays when the numbers are outside the range
+else: 
+    print("That is not a number.") #displays when the value is a string and not an integer 
+
+#ADVANCED REQUIRMENT : LEAP YEAR 
+months = {1 : 31, 
+          2 : 28, 
+          3 : 31, 
+          4 : 30, 
+          5 : 31, 
+          6 : 30,
+          7 : 31, 
+          8 : 31, 
+          9 : 30, 
+          10 : 31, 
+          11 : 30, 
+          12 : 31}
+
+months2 = input("Input the month number! : ") #asks the user to input a number from the dictionary (1-12)
+
+if months2.isdigit():
+    monthnumber = int(months2) 
+    
+    if 1<= monthnumber <=12 :
+        if monthnumber == 2: #if the user input equals to 2, code proceeds to the 2nd user input
+            leap = input("is the year a leap year? YES/NO : ") #asks user if the year is a leap year 
+            if leap == "YES": #if input equals to "YES" the code proceeds with first condition
+                print("Month 2 has 29 days.") #displays when leap == "YES
+            else: 
+                print("Month 2 has 28 days.") #displays when leap == "NO  
+        else: 
+            print(f"Month {monthnumber} has {months[monthnumber]} days.") 
+    else: 
+        print("That is not a valid month")
+else: 
+    print("That is not a number.")
